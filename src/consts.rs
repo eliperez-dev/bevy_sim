a92e1f77b9953257b1d7a6a1b8085fe6ebce5120
Eli <eliperez0024@gmail.com>
2026-02-16 14:36:53 -0800

    adding new atmoshpere renderer

--- src/consts.rs
+++ src/consts.rs
@@ -1,6 +1,6 @@
 
 pub const CHUNK_SIZE: f32 = 1000.0; 
-pub const TERRAIN_QUALITY: f32 = 30.0;
+pub const TERRAIN_QUALITY: f32 = 20.0;
 pub const MAP_HEIGHT_SCALE: f32 = 130.0;
 pub const TERRAIN_SMOOTHNESS: f32 = 0.5;
 
@@ -9,5 +9,5 @@ pub const FOG_DISTANCE: f32 = 500.0;
 
 pub const COMPUTE_SMOOTH_NORMALS: bool = false;
 
-pub const RENDER_DISTANCE: i32 = 25;
+pub const RENDER_DISTANCE: i32 = 5;
 pub const DESPAWN_DISTANCE: i32 = RENDER_DISTANCE + 1;
